da031833238069af76805053b76e12188afd4c99
Valéria <vvqf2016@gmail.com>
2024-08-15 20:47:41 -0300

    chore: added inputs into a page

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,3 @@
-import CpfInput from "@/components/CpfInput";
-import DateInput from "@/components/DateInput";
-import { EmailInput } from "@/components/EmailInput";
-import { NameInput } from "@/components/NameInput";
-import PasswordInput from "@/components/PasswordInput";
-import { PhoneNumberInput } from "@/components/PhoneNumber";
 import { View } from "react-native";
 
 export default function Index() {
@@ -13,14 +7,9 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap:10
       }}
     >
-      <DateInput label='Date of birth' width={316} />
-      <NameInput label='Name' placeholder='John Doe'/>
-      <EmailInput label='Email' placeholder="johndoe@email.com"/>
-      <PasswordInput label='Password' placeholder='********'/>
-      <CpfInput label='Cpf' placeholder="000.000.000-"/>
-      <PhoneNumberInput label='Phone Number' placeholder='(00) 0000-0000'/>
   
     </View>
   );
