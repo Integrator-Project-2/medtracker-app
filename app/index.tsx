a4c84dfbde7c97fcd94cc92e1a009e7629aa6643
KarlaJulyana <karla@solarz.com.br>
2024-08-14 22:54:49 -0300

    chore: delete unused files and create primary button

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { PrimaryButton } from "@/components/PrimaryButton";
+import { View, Image } from "react-native";
 
 export default function Index() {
   return (
@@ -7,9 +8,39 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 10
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <PrimaryButton
+        text="Get started"
+        bgColor="#354DB0"
+        width={316} 
+        height={52}
+        onPress={() => console.log('Button Pressed')}
+      />
+
+      <PrimaryButton
+        text="I already have an account"
+        bgColor="transparent"
+        width={316} 
+        height={52}
+        textColor="#4D80F9"
+        border="1px solid #4D80F9"
+        onPress={() => console.log('Button Pressed')}
+      />
+
+      <PrimaryButton
+        text="Sign up with Google"
+        bgColor="transparent"
+        width={316} 
+        height={52}
+        textColor="#4D80F9"
+        icon={
+          <Image source={require('../assets/images/devicon_google.png')} style={{ marginRight: 10 }} />
+        }
+        border="1px solid #4D80F9"
+        onPress={() => console.log('Button Pressed')}
+      />
     </View>
   );
 }
---
+++ components/PrimaryButton/index.tsx
@@ -0,0 +1,42 @@
+import { GestureResponderEvent, Image } from "react-native";
+import { StyledPrimaryButton, StyledTextButton } from "./styles";
+
+interface PrimaryButtonProps {
+    text: string;
+    icon?: React.ReactNode;
+    bgColor?: string;
+    textColor?: string;
+    width?: number | string;
+    height?: number | string;
+    border?: string;
+    onPress: (event: GestureResponderEvent) => void;
+  }
+
+export function PrimaryButton({ 
+    text,
+    icon,
+    bgColor,
+    textColor,
+    width,
+    height,
+    border,
+    onPress,
+ }: PrimaryButtonProps) {
+    return (
+        <StyledPrimaryButton
+            bgColor={bgColor} 
+            width={width} 
+            height={height} 
+            border={border}
+            onPress={onPress}
+        >
+            {icon && icon}
+
+            <StyledTextButton
+                color={textColor}    
+            >
+                    {text}
+            </StyledTextButton>
+        </StyledPrimaryButton>
+    )
+}
