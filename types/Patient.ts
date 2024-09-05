885b139ab30e6c14ffa8fd04931ad38252110c38
Valéria <vvqf2016@gmail.com>
2024-09-05 16:08:08 -0300

    chore: added patient type

--- types/Patient.ts
+++ types/Patient.ts
@@ -1,1 +1,9 @@
-sc
+interface Patient {
+    id: number;
+    cpf: string;
+    user: User;
+    gender: 'M' | 'F' | 'NB';
+    height?: number;
+    weight?: number;
+    allergies_and_observations?: string;
+}
