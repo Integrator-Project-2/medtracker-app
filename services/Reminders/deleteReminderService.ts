21bc49c2f368a0a2dcf4edb7b18f0123eb664f17
Valéria <vvqf2016@gmail.com>
2024-09-08 01:05:39 -0300

    chore: delete service ajusts

--- services/Reminders/deleteReminderService.ts
+++ services/Reminders/deleteReminderService.ts
@@ -1,12 +1,19 @@
 import axios from 'axios';
 
-const API_URL = 'http://10.0.2.2:8000/api/medication-reminder/';
 
 export const deleteReminder = async (id: number) => {
     try {
-        await axios.delete(`${API_URL}${id}/`);
+        const response = await axios.delete(`http://10.0.2.2:8000/api/medication-reminder/${id}/`);
+        if (response.status === 204) {
+            console.log('Reminder deleted successfully');
+        } else {
+            console.error('Unexpected response status:', response.status);
+        }
     } catch (error) {
-        console.error('Error deleting reminder:', error);
-        throw error;
+        if (axios.isAxiosError(error)) {
+            console.error('Axios error:', error.message);
+        } else {
+            console.error('Unexpected error:', error);
+        }
     }
 };
