268e3bcbefa5f598241835c33c8cfd53bb390809
Valéria <vvqf2016@gmail.com>
2024-09-08 21:22:36 -0300

    chore: altered prescription service to gateway

--- services/Prescriptions/prescriptionService.ts
+++ services/Prescriptions/prescriptionService.ts
@@ -1,21 +1,33 @@
 import { Doctor } from '@/types/Doctor';
 import { Prescription } from '@/types/Prescription';
-import axios from 'axios';
-
-const API_BASE_URL = "http://10.0.2.2:8003/api";
-const DOCTOR_SERVICE_URL = "http://10.0.2.2:8001/api";
+import api from '../api';
 
 
 export const fetchPrescriptions = async (patientId: number): Promise<Prescription[]> => {
-    const response = await axios.get<Prescription[]>(`${API_BASE_URL}/prescriptions/patient/${patientId}/`);
-    return response.data;
+    try {
+
+        const url = `/prescriptions/patient/${patientId}/`
+
+        const response = await api.get<Prescription[]>(url);
+        return response.data;
+    } catch (error) {
+        console.error("Erro ao buscar prescrições:", error);
+        throw new Error("Não foi possível buscar as prescrições.");
+    }
 };
 
+
 export const fetchDoctorDetails = async (doctorId: number): Promise<{ name: string, specialty: string }> => {
-    const response = await axios.get<Doctor>(`${DOCTOR_SERVICE_URL}/doctors/${doctorId}/`);
-    const doctor = response.data;
-    return {
-        name: doctor.user.name,
-        specialty: doctor.specialty,
-    };
+    try {
+        const url = `/doctors-service/${doctorId}/`
+        const response = await api.get<Doctor>(url);
+        const doctor = response.data;
+        return {
+            name: doctor.user.name,
+            specialty: doctor.specialty,
+        };
+    } catch (error) {
+        console.error("Erro ao buscar detalhes do médico:", error);
+        throw new Error("Não foi possível buscar os detalhes do médico.");
+    }
 };
